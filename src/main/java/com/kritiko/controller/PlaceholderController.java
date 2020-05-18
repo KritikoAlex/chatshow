67206e4c6e2763cb0be0ad380b8f8eaa7a9068a8
Alexander Kaiser <alexander.kaiser@uni-bremen.de>
2020-05-18 02:17:15 +0200

    Added more functionality regarding multiple pokemons

--- src/main/java/com/kritiko/controller/PlaceholderController.java
+++ src/main/java/com/kritiko/controller/PlaceholderController.java
@@ -5,8 +5,11 @@ import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.storage.PlayerPartyStorage;
 import com.pixelmonmod.pixelmon.util.PixelmonPlayerUtils;
 import net.minecraft.entity.player.EntityPlayerMP;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.HandTypes;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.message.MessageChannelEvent;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
 import org.spongepowered.api.text.format.TextColors;
@@ -27,40 +30,65 @@ public class PlaceholderController {
      * @param p     The player that sent this message
      */
     public void manageChatEvent(MessageChannelEvent.Chat event, Player p) {
-        EntityPlayerMP mp = PixelmonPlayerUtils.getUniquePlayerStartingWith(p.getName());
-        if (mp == null) return;
-        PlayerPartyStorage pps = Pixelmon.storageManager.getParty(mp);
+        if (!(p instanceof EntityPlayerMP)) return;
+        EntityPlayerMP mp = (EntityPlayerMP) p;
+
+        // This section is to display the currently held item in chat
+        if (event.getRawMessage().toPlain().matches("(.*)\\{item}(.*)")) {
+            if (p.getItemInHand(HandTypes.MAIN_HAND).isPresent() && !p.getItemInHand(HandTypes.MAIN_HAND).get().isEmpty()) {
+                Text text = Text.builder()
+                        .color(TextColors.GOLD)
+                        .style(TextStyles.BOLD)
+                        .append(Text.of(p.getItemInHand(HandTypes.MAIN_HAND).get().getTranslation().get()))
+                        .onHover(TextActions.showItem(p.getItemInHand(HandTypes.MAIN_HAND).get().createSnapshot()))
+                        .build();
+                event.setMessage(Text.of(event.getMessage().replace(Pattern.compile("\\{item}"), text)));
+            } else {
+                p.sendMessage(Text.of(TextColors.RED, "You do not have an item in your hand!"));
+                event.setCancelled(true);
+                return;
+            }
+        }
 
         //Check if this message contains a valid pokemon placeholder
         //Only messages that contain a '{pokemon*NUM*}' where NUM equals 1-6
         if (!event.getRawMessage().toPlain().matches("(.*)\\{pokemon[1-6]}(.*)")) return;
-        String num = event.getRawMessage().toPlain().split("\\{pokemon")[1].split("}")[0];
-        //Gets converted to the number that is used internally because internally the list begins at index 0 instead of 1
-        int number = Integer.parseInt(num) - 1;
 
-        //Check if this slot even contains a pokemon
-        if (pps.get(number) == null) {
-            p.sendMessage(Text.of(TextColors.RED, "There is no pokemon in slot " + (number + 1) + "!"));
-            event.setCancelled(true);
-            return;
+        PlayerPartyStorage pps = Pixelmon.storageManager.getParty(mp);
+        Text message = event.getMessage();
+
+        while (Pattern.matches("(.*)\\{pokemon[1-6]}(.*)", message.toPlain())) {
+            String num = message.toPlain().split("\\{pokemon", 2)[1].split("}", 2)[0];
+            //Gets converted to the number that is used internally because internally the list begins at index 0 instead of 1
+            int number = Integer.parseInt(num) - 1;
+
+            //Check if this slot even contains a pokemon
+            if (pps.get(number) == null) {
+                p.sendMessage(Text.of(TextColors.RED, "There is no pokemon in slot " + (number + 1) + "!"));
+                event.setCancelled(true);
+                return;
+            }
+
+            //The hover message
+            Text.Builder builder = Text.builder();
+            ChatShow.hoverMessages.forEach(x -> {
+                String replaced = x.replaceAll("<prefix>", "%party_" + (number + 1));
+                builder.append(ChatShow.getInstance().getPlaceholderService().replacePlaceholders(replaced, p, p)).append(Text.NEW_LINE);
+            });
+
+            //The pokemon text with the hover effect
+            Text text = Text.builder()
+                    .color(TextColors.GOLD)
+                    .style(TextStyles.BOLD)
+                    .append(ChatShow.getInstance().getPlaceholderService().replacePlaceholders("%party_" + (number + 1) + "_nickname%", p, p))
+                    .onHover(TextActions.showText(builder.build()))
+                    .build();
+
+            //Replace the pokemon placeholder with the actual text with the hover message
+            message = message.replace(Pattern.compile("\\{pokemon[" + (number + 1) + "]}"), text);
+            System.out.println(message.replace(Pattern.compile("\\{pokemon[" + (number + 1) + "]}"), text).toPlain());
+            System.out.println(message.toPlain());
         }
-
-        //The hover message
-        Text.Builder builder = Text.builder();
-        ChatShow.hoverMessages.forEach(x -> {
-            String replaced = x.replaceAll("<prefix>", "%party_" + (number + 1));
-            builder.append(ChatShow.getInstance().getPlaceholderService().replacePlaceholders(replaced, p, p)).append(Text.NEW_LINE);
-        });
-
-        //The pokemon text with the hover effect
-        Text text = Text.builder()
-                .color(TextColors.GOLD)
-                .style(TextStyles.BOLD)
-                .append(ChatShow.getInstance().getPlaceholderService().replacePlaceholders("%party_" + (number + 1) + "_nickname%", p, p))
-                .onHover(TextActions.showText(builder.build()))
-                .build();
-
-        //Replace the pokemon placeholder with the actual text with the hover message
-        event.setMessage(Text.of(event.getMessage().replace(Pattern.compile("\\{pokemon[1-6]}"), text)));
+        event.setMessage(Text.of(message));
     }
 }
